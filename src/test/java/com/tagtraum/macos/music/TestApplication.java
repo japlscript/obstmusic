de8c7bd40989274e7af42e7d1ca2f4d282e55df2
Hendrik Schreiber <hs@tagtraum.com>
2021-09-28 12:45:24 +0200

    Added get current artwork example.

--- src/test/java/com/tagtraum/macos/music/TestApplication.java
+++ src/test/java/com/tagtraum/macos/music/TestApplication.java
@@ -6,8 +6,15 @@
  */
 package com.tagtraum.macos.music;
 
+import com.tagtraum.japlscript.Reference;
 import com.tagtraum.japlscript.execution.JaplScriptException;
+import com.tagtraum.japlscript.language.Tdta;
 import org.junit.jupiter.api.Test;
+
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
 
 import static org.junit.jupiter.api.Assertions.assertFalse;
 import static org.junit.jupiter.api.Assertions.assertNotNull;
@@ -35,13 +42,62 @@ public class TestApplication {
     }
 
     @Test
-    public void testCurrentlyPLaying() {
+    public void testCurrentlyPlaying() {
         final Application application = Application.getInstance();
         try {
             final Track currentTrack = application.getCurrentTrack();
             System.out.println("Current track: " + currentTrack.getName() + " by " + currentTrack.getArtist());
         } catch(JaplScriptException e) {
             System.out.println("No track loaded (we assume).");
+        }
+    }
+
+    @Test
+    public void testCurrentlyPlayingArtwork() throws IOException {
+        final Application application = Application.getInstance();
+        try {
+            final Track currentTrack = application.getCurrentTrack();
+            final Artwork[] artworks = currentTrack.getArtworks();
+            if (artworks.length > 0) {
+                final Artwork artwork = artworks[0];
+                final Reference rawData = artwork.getRawData();
+                if (rawData != null) {
+
+                    // get image data
+                    final Tdta tdta = rawData.cast(Tdta.class);
+                    final byte[] imageData = tdta.getTdta();
+
+                    // get image format
+                    final String format = artwork.getFormat().getObjectReference().toLowerCase();
+                    final String extension;
+                    if (format.contains("png")) extension = ".png";
+                    else if (format.contains("tiff")) extension = ".tiff";
+                    else if (format.contains("gif")) extension = ".gif";
+                    else if (format.contains("bmp")) extension = ".bmp";
+                    else if (format.contains("pdf")) extension = ".pdf";
+                    else extension = ".jpeg";
+                    
+                    // write image file
+                    final Path artworkPath = Paths.get("current_artwork" + extension);
+                    System.out.println("Writing artwork to file " + artworkPath);
+                    Files.write(artworkPath, imageData);
+                }
+            }
+        } catch(JaplScriptException e) {
+            System.out.println("No track loaded (we assume).");
+        }
+    }
+
+    @Test
+    public void testGetTrackProperties() {
+        // this is more demo than test code...
+        final Application application = Application.getInstance();
+        try {
+            final LibraryPlaylist library = application.getSources()[0].getLibraryPlaylists()[0];
+            System.out.println(library.getTrack(0).getProperties());
+        } catch(JaplScriptException e) {
+            // oops
+            e.printStackTrace();
         }
     }
 }
